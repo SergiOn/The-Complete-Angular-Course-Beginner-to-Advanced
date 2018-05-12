5c9cb6950d5f5e0fac337194319ead5e11c9b49d
SergiOn <onishhenko@gmail.com>
2018-05-12 21:58:48 +0300

    section #4 #lecture-42 # Adding Bootstrap

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -10,6 +10,7 @@ import { Component } from '@angular/core';
         <td [attr.colspan]="colSpan"></td>
       </tr>
     </table>
+    <button class="btn badge-primary">Save</button>
   `
 })
 export class CoursesComponent {
