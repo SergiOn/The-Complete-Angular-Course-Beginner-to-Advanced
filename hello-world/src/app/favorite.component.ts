6ae850e5cae59263bd6899aa1bdcd13ece3d9c73
SergiOn <onishhenko@gmail.com>
2018-05-13 23:41:06 +0300

    section #5 #lecture-54 # Aliasing Input Properties

--- hello-world/src/app/favorite.component.ts
+++ hello-world/src/app/favorite.component.ts
@@ -3,12 +3,15 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'favorite',
   template: `
-    <div>isFavorite: {{isFavorite | json}}</div>
+    <!--<div>isFavorite: {{isFavorite | json}}</div>-->
+    <div>isFavorite: {{isSelected | json}}</div>
   `,
-  inputs: ['isFavorite']
+  // inputs: ['isFavorite']
 })
 export class FavoriteComponent {
   // @Input() isFavorite: boolean;
-  isFavorite: boolean;
+  // isFavorite: boolean;
+  @Input('isFavorite') isSelected: boolean;
+
 
 }
